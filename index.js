f268b239451047790faffd3ac63eef9754fd2498
Mano Sriram <mano.sriram0@gmail.com>
2022-09-18 01:02:58 +0530

    remove env port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const PORT = 5002;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello!");
+  res.send("Hello!");
 });
 
 app.listen(PORT, () => console.log(`Server at ${PORT}`));
